f3f62347954586cbc0e6c6dfe834249a3849f15f
craig-PC\craig <craigegodfrey@gmail.com>
2018-11-13 18:38:41 -0600

    initial files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SettingsComponent } from './components/forms/settings/settings.component';
+import { AddMealComponent } from './components/forms/add-meal/add-meal.component';
+import { PlateComponent } from './components/objects/plate/plate.component';
+import { MenuComponent } from './components/objects/menu/menu.component';
+import { MainComponent } from './components/pages/main/main.component';
+import { ToolbarComponent } from './components/objects/toolbar/toolbar.component';
+import { EditMealComponent } from './components/forms/edit-meal/edit-meal.component';
+import { TitlebarComponent } from './components/objects/titlebar/titlebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent,
+    AddMealComponent,
+    PlateComponent,
+    MenuComponent,
+    MainComponent,
+    ToolbarComponent,
+    EditMealComponent,
+    TitlebarComponent
   ],
   imports: [
     BrowserModule,
